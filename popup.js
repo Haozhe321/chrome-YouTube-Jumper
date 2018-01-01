e0472445d2305efc941d158ef2c71b607ef96810
Haozhe <phaozhe@hotmail.com>
2018-01-01 23:04:29 +0800

    Fix issue with copying URL

--- popup.js
+++ popup.js
@@ -1,14 +1,4 @@
-var actualUrl;
-function getActualUrl() {
-    chrome.tabs.query({
-        'active': true,
-        'windowId': chrome.windows.WINDOW_ID_CURRENT
-    }, function (tabs) {
-        var urlArray = tabs[0].url.split("&t");
-        var firstUrl = urlArray[0].split("#t");
-        actualUrl = firstUrl;
-    });
-}
+var actualUrl; //to store the actal url of the website without the timestamp 
 document.addEventListener('DOMContentLoaded', function() {
     chrome.tabs.query({
         'active': true,
@@ -55,30 +45,35 @@ function jumpTime() {
 
     return time;
 }
-
+window.onload=function(){
+    var copyUrl = document.getElementById("copy-url");
+    copyUrl.addEventListener('click', function(event) {
+      copyURLToClipboard();
+    });
+}
 
 function copyURLToClipboard() {
     var textArea = document.createElement("textarea");
-    // Place in top-left corner of screen regardless of scroll position.
-    textArea.style.position = 'fixed';
-    textArea.style.top = 0;
-    textArea.style.left = 0;
-
-    // Ensure it has a small width and height. Setting to 1px / 1em
-    // doesn't work as this gives a negative w/h on some browsers.
-    textArea.style.width = '2em';
-    textArea.style.height = '2em';
-
-    // We don't need padding, reducing the size if it does flash render.
-    textArea.style.padding = 0;
-
-    // Clean up any borders.
-    textArea.style.border = 'none';
-    textArea.style.outline = 'none';
-    textArea.style.boxShadow = 'none';
-
-    // Avoid flash of white box if rendered for any reason.
-    textArea.style.background = 'transparent';
+    // // Place in top-left corner of screen regardless of scroll position.
+    // textArea.style.position = 'fixed';
+    // textArea.style.top = 0;
+    // textArea.style.left = 0;
+    //
+    // // Ensure it has a small width and height. Setting to 1px / 1em
+    // // doesn't work as this gives a negative w/h on some browsers.
+    // textArea.style.width = '2em';
+    // textArea.style.height = '2em';
+    //
+    // // We don't need padding, reducing the size if it does flash render.
+    // textArea.style.padding = 0;
+    //
+    // // Clean up any borders.
+    // textArea.style.border = 'none';
+    // textArea.style.outline = 'none';
+    // textArea.style.boxShadow = 'none';
+    //
+    // // Avoid flash of white box if rendered for any reason.
+    // textArea.style.background = 'transparent';
 
 
     textArea.value = actualUrl;
