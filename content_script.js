1fa246a07ba6d88c29b86fb885a2567991f181af
Haozhe <phaozhe@hotmail.com>
2018-01-04 16:23:44 +0800

    Add function to find occurance of words in subtitle

--- content_script.js
+++ content_script.js
@@ -13,14 +13,27 @@ function chooseLanguages() {
     chooseLang[0].click();
     setTimeout(selectEnglish, 500);
 }
-function selectEnglish() {
 
+function selectEnglish() {
     var languageList = document.getElementsByClassName("yt-ui-menu-item yt-uix-menu-close-on-select yt-uix-button-menu-item");
     for(var i = 0; i < languageList.length; i++) {
         if(languageList[i].value == "en: English - .en" ||
             languageList[i].value == "en: English (Automatic Captions) - a.en") {
             languageList[i].click();
             break;
+        }
+    }
+    setTimeout(getListOfOccurances("he"), 500);
+}
+
+function getListOfOccurances(text) {
+    var captionLineList = document.getElementsByClassName("caption-line");
+    var occurances = []; //occurances contain a list of time that this text occurs
+    for(var i = 1; i < captionLineList.length; i++) {
+        alert(captionLineList[i].getAttribute("data-time"));
+        var string = captionLineList[i].getElementsByClassName("caption-line-text")[0].innerHTML;
+        if(string.includes(text)) {
+            occurances.push(time);
         }
     }
 }
